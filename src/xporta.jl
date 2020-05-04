385b052d21382d47ae05cd5f0b3f6814f7281622
Brian Doolittle <briand4@illinois.edu>
2020-05-03 21:03:39 -0500

    docs for traf

--- src/xporta.jl
+++ src/xporta.jl
@@ -30,6 +30,47 @@ function run_xporta(method_flag::String, args::Array{String,1}; suppress::Bool=t
 end
 
 """
+Transforms the vertex representation of a polyhedron into the intersecting
+half-space representation and vice verse. The `traf` method computes an `IEQ` struct
+given a `POI` struct,
+
+    traf( poi::POI; kwargs... ) :: IEQ
+
+or computes the `POI` struct from the `IEQ` struct.
+
+    traf(ieq::IEQ; kwargs... ) :: POI
+
+where `kwargs` is shorthand for the following keyword arguments:
+
+* `cleanup :: Bool = true` - Remove created files after computation.
+* `dir :: String = "./"` - The directory in which to write files.
+* `filename :: String = "traf_tmp"`- The name of produced files
+* `opt_flag :: String = ""` - Optional flags to pass the `traf` method of the xporta binary.
+
+The following excerpt from the PORTA documentation lists the valid flags and their behavior:
+
+        -p     Unbuffered redirection of terminal messages into  file filename_'.prt'
+
+        -o     Use  a heuristic to eliminate that variable  next,  for which the number of new inequalities is
+               minimal (local criterion). If this option is set, inequalities  which are  recognized  to  be
+               facet-inducing  for the finite linear system are printed into a  file as soon as they
+               are identified.
+
+        -c     Fourier-Motzkin elimination without using the rule  of Chernikov
+
+        -s     Appends a statistical  part  to  each  line  with  the number  of coefficients
+
+        -v     Printing a   table in the  output file which indicates strong validity
+
+        -l     Use  a  special  integer arithmetic allowing the integers to have arbitrary lengths. This
+               arithmetic is not as efficient as the system's integer arithmetic with respect to time and
+               storage requirements.  Note: Output values which exceed the 32-bit integer storage size
+               are written in hexadecimal format (hex). Such hexadecimal format can not be reread as input.
+
+!!! note
+    By default files created by the `xporta` binary are deleted. For longer computations with
+    xporta, it may be desired that intermediate files are not deleted. Passing the argument
+    `cleanup = false` will cause the `traf` method to write all files to directroy `dir`.
 """
 function traf(poi::POI; dir::String="./", filename::String="traf_tmp", opt_flag::String="", cleanup::Bool=true) :: IEQ
     xporta_args = Array{String,1}(undef,0)
